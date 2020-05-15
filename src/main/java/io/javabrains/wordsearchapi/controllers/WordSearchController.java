d4e69cf49a2afecea998588a2269759c032eb4e7
Koushik Kothagal <koush.s@gmail.com>
2020-05-15 15:21:37 -0700

    Day 2: Added JS frontend

--- src/main/java/io/javabrains/wordsearchapi/controllers/WordSearchController.java
+++ src/main/java/io/javabrains/wordsearchapi/controllers/WordSearchController.java
@@ -15,6 +15,7 @@ public class WordSearchController {
     WordGridService wordGridService;
 
     @GetMapping("/wordgrid")
+    @CrossOrigin(origins = "http://localhost:1234")
     public String createWordGrid(@RequestParam int gridSize, @RequestParam String wordList) {
         List<String> words = Arrays.asList(wordList.split(","));
         char[][] grid = wordGridService.generateGrid(gridSize, words);
--- src/main/java/io/javabrains/wordsearchapi/services/WordGridService.java
+++ src/main/java/io/javabrains/wordsearchapi/services/WordGridService.java
@@ -42,8 +42,8 @@ public class WordGridService {
                 }
             }
 
-            Collections.shuffle(coordinates);
             for (String word: words) {
+                Collections.shuffle(coordinates);
                 for (Coordinate coordinate : coordinates) {
                     int x = coordinate.x;
                     int y = coordinate.y;
@@ -132,37 +132,43 @@ public class WordGridService {
                 case HORIZONTAL:
                     if (coordinate.y + wordLength > gridSize) return false;
                     for (int i = 0; i < wordLength; i++) {
-                        if (contents[coordinate.x][coordinate.y + i] != '_') return false;
+                        char letter = contents[coordinate.x][coordinate.y + i];
+                        if (letter != '_' && letter != word.charAt(i)) return false;
                     }
                     break;
                 case VERTICAL:
                     if (coordinate.x + wordLength > gridSize) return false;
                     for (int i = 0; i < wordLength; i++) {
-                        if (contents[coordinate.x + i][coordinate.y] != '_') return false;
+                        char letter = contents[coordinate.x + i][coordinate.y];
+                        if (letter != '_' && letter != word.charAt(i)) return false;
                     }
                     break;
                 case DIAGONAL:
                     if (coordinate.x + wordLength > gridSize || coordinate.y + wordLength > gridSize) return false;
                     for (int i = 0; i < wordLength; i++) {
-                        if (contents[coordinate.x + i][coordinate.y + i] != '_') return false;
+                        char letter = contents[coordinate.x + i][coordinate.y + i];
+                        if (letter != '_' && letter != word.charAt(i)) return false;
                     }
                     break;
                 case HORIZONTAL_INVERSE:
                     if (coordinate.y < wordLength) return false;
                     for (int i = 0; i < wordLength; i++) {
-                        if (contents[coordinate.x][coordinate.y - i] != '_') return false;
+                        char letter = contents[coordinate.x][coordinate.y - i];
+                        if (letter != '_' && letter != word.charAt(i)) return false;
                     }
                     break;
                 case VERTICAL_INVERSE:
                     if (coordinate.x < wordLength) return false;
                     for (int i = 0; i < wordLength; i++) {
-                        if (contents[coordinate.x - i][coordinate.y] != '_') return false;
+                        char letter = contents[coordinate.x - i][coordinate.y];
+                        if (letter != '_' && letter != word.charAt(i)) return false;
                     }
                     break;
                 case DIAGONAL_INVERSE:
                     if (coordinate.x < wordLength || coordinate.y < wordLength) return false;
                     for (int i = 0; i < wordLength; i++) {
-                        if (contents[coordinate.x - i][coordinate.y - i] != '_') return false;
+                        char letter = contents[coordinate.x - i][coordinate.y - i];
+                        if (letter != '_' && letter != word.charAt(i)) return false;
                     }
                     break;
             }
